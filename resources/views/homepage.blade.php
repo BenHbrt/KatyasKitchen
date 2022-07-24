dcdc5d0f46fcb96697a764f7a18ed276eb664624
BenHbrt <benherbert1983@googlemail.com>
2022-07-24 17:53:48 +0200

    Initialisation

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sbeerka</title>
+    <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js" defer></script>
+    <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" defer></script>
+    <script src="https://unpkg.com/react-table@7.8.0/dist/react-table.development.js" defer></script>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+
+    <div id="root">
+
+    </div>
+    <script src="{{ mix('/js/index.js') }}"></script>
+
+</body>
+
+</html>

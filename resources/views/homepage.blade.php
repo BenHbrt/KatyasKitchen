3fcace153d3acad87e4d776ee02988ffa9645639
BenHbrt <benherbert1983@googlemail.com>
2022-07-24 18:05:44 +0200

    Image folder

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sbeerka</title>
-    <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js" defer></script>
-    <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" defer></script>
-    <script src="https://unpkg.com/react-table@7.8.0/dist/react-table.development.js" defer></script>
+    <title>Katya's Kitchen</title>
+    {{-- <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js" defer></script>
+    <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" defer></script> --}}
+    {{-- <script src="https://unpkg.com/react-table@7.8.0/dist/react-table.development.js" defer></script> --}}
     <link rel="stylesheet" href="css/app.css">
+    <link rel="icon" href="img/assets/Restaurant.png">
 </head>
 
 <body>

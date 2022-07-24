0ba9040bb0c92d1c3ef2280f2326691e9aa46ef8
BenHbrt <benherbert1983@googlemail.com>
2022-07-24 18:38:08 +0200

    Title

--- resources/js/Index/App.js
+++ resources/js/Index/App.js
@@ -1,6 +1,12 @@
 const App = () => {
     return (
-        <h1>Katya's Kitchen</h1>
+        <div className="page">
+            <div className="title_bar">
+            <h1>Katya's Kitchen</h1>
+            <img src="img/assets/Title.png" alt="Plate and cutlery" />
+            </div>
+        </div>
+        
     )
 }
 export default App;
